092cf88f8b0441b7139c12ca696f4352374691f2
Yander Sanchez <sanchezyander@gmail.com>
2025-06-12 18:56:16 -0400

    study: add class constructors, primary and secondary

--- codelab/june-12/classes-and-objects/src/example/myapp/Aquarium.kt
+++ codelab/june-12/classes-and-objects/src/example/myapp/Aquarium.kt
@@ -1,15 +1,31 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 32
-    var height: Int = 54
-    var length: Int = 130
+class Aquarium(var length: Int = 32, var width: Int = 54, var height: Int = 130) {
+    init  {
+        println("aquarium initializing")
+    }
+
+//    init {
+//        val volume = width * height * length / 1000
+//        println("Volume: $volume l")
+//    }
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
 
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
-        val volume = width * height * length / 1000
+
         println("Volume: $volume l")
     }
 }
--- codelab/june-12/classes-and-objects/src/example/myapp/Main.kt
+++ codelab/june-12/classes-and-objects/src/example/myapp/Main.kt
@@ -5,6 +5,16 @@ fun buildAquarium() {
     myAquarium.printSize()
     myAquarium.height = 80
     myAquarium.printSize()
+    val aquariumBeta = Aquarium(height = 56, length = 160)
+    aquariumBeta.volume = 900
+    aquariumBeta.printSize()
+
+//    using secondary constructor
+    println("Using secondary constructor...")
+    val aquariumTrench = Aquarium(numberOfFish = 30)
+    aquariumTrench.printSize()
+    val volume = aquariumTrench.width * aquariumTrench.length * aquariumTrench.height / 1000
+    println("Volume: $volume l.")
 }
 
 fun main() {
